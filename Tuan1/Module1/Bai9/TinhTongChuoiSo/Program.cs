b482062431f672174c757b3bd7ba05adb6bab0f5
vanthuan0306 <vanthuan0306@users.noreply.github.com>
2020-03-13 19:57:45 +0700

    sửa bài

---
+++ Tuan1/Module1/Bai9/TinhTongChuoiSo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TinhTongChuoiSo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, n;
+            Console.WriteLine("***TINH TONG DE QUY***");
+            Console.Write("Nhap x : ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap y : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("S({0},{1}) = {2}", x, n, Math.Round(TinhTongDeQuy(x, n), 3));
+            Console.ReadLine();
+        }
+
+        private static int GiaiThua(int n)
+        {
+            if (n == 1) return 1;
+            else return n * GiaiThua(n - 1);
+        }
+
+        private static double LuyThua(double coSo, double soMu)
+        {
+            if (coSo == 1) return 1;
+            else return Math.Pow(coSo, soMu);
+        }
+
+        private static double TinhTongDeQuy(int x, int n)
+        {
+            if (n == 1) return x;
+            else return LuyThua(x, n) / GiaiThua(n) + TinhTongDeQuy(x, n - 1);
+        }
+    }
+}
---
+++ Tuan1/Module1/SoGaSoCho/SoGaSoCho/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SoGaSoCho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***GIAI BAI TAP SO GA, SO CHO***");
+            double a1, a2, b1, b2, c1, c2, d, dx, dy;
+            Console.Write("Nhap a1 : ");
+            a1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap b1 : ");
+            b1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap c1 : ");
+            c1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap a2 : ");
+            a2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap b2 : ");
+            b2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap c2 : ");
+            c2 = Convert.ToInt32(Console.ReadLine());
+            d = a1 * b2 - a2 * b1;
+            dx = c1 * b2 - c2 * b1;
+            dy = a1 * c2 - a2 * c1;
+            Console.WriteLine("x = " + dx / d);
+            Console.WriteLine("y = " + dy / d);
+            Console.ReadLine();
+        }
+    }
+}
