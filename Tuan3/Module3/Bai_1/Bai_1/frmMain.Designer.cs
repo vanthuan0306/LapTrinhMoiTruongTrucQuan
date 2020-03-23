1ef95bb9f970852bf4633c74c17f15aff046b4a5
vanthuan0306 <vanthuan0306@users.noreply.github.com>
2020-03-23 07:04:00 +0700

    module 3

---
+++ Tuan3/Module3/Bai_1/Bai_1/frmMain.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Bai_1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtThanhTien = new System.Windows.Forms.TextBox();
+            this.txtDonGia = new System.Windows.Forms.TextBox();
+            this.txtSoLuong = new System.Windows.Forms.TextBox();
+            this.txtTenHang = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnKetThuc = new System.Windows.Forms.Button();
+            this.btnTiep = new System.Windows.Forms.Button();
+            this.btnThanhToan = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtThanhTien);
+            this.groupBox1.Controls.Add(this.txtDonGia);
+            this.groupBox1.Controls.Add(this.txtSoLuong);
+            this.groupBox1.Controls.Add(this.txtTenHang);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(31, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(328, 156);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtThanhTien
+            // 
+            this.txtThanhTien.Location = new System.Drawing.Point(94, 115);
+            this.txtThanhTien.Name = "txtThanhTien";
+            this.txtThanhTien.ReadOnly = true;
+            this.txtThanhTien.Size = new System.Drawing.Size(146, 20);
+            this.txtThanhTien.TabIndex = 7;
+            this.txtThanhTien.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtDonGia
+            // 
+            this.txtDonGia.Location = new System.Drawing.Point(94, 80);
+            this.txtDonGia.Name = "txtDonGia";
+            this.txtDonGia.Size = new System.Drawing.Size(146, 20);
+            this.txtDonGia.TabIndex = 6;
+            this.txtDonGia.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtSoLuong
+            // 
+            this.txtSoLuong.Location = new System.Drawing.Point(94, 50);
+            this.txtSoLuong.Name = "txtSoLuong";
+            this.txtSoLuong.Size = new System.Drawing.Size(146, 20);
+            this.txtSoLuong.TabIndex = 5;
+            this.txtSoLuong.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtSoLuong.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSoLuong_KeyPress);
+            // 
+            // txtTenHang
+            // 
+            this.txtTenHang.Location = new System.Drawing.Point(94, 17);
+            this.txtTenHang.Name = "txtTenHang";
+            this.txtTenHang.Size = new System.Drawing.Size(146, 20);
+            this.txtTenHang.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Thành Tiền";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Đơn Giá";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số Lượng";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên Hàng";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnKetThuc);
+            this.groupBox2.Controls.Add(this.btnTiep);
+            this.groupBox2.Controls.Add(this.btnThanhToan);
+            this.groupBox2.Location = new System.Drawing.Point(31, 204);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(328, 52);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnKetThuc
+            // 
+            this.btnKetThuc.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnKetThuc.Location = new System.Drawing.Point(230, 19);
+            this.btnKetThuc.Name = "btnKetThuc";
+            this.btnKetThuc.Size = new System.Drawing.Size(75, 23);
+            this.btnKetThuc.TabIndex = 2;
+            this.btnKetThuc.Text = "Kết Thúc";
+            this.btnKetThuc.UseVisualStyleBackColor = true;
+            this.btnKetThuc.Click += new System.EventHandler(this.btnKetThuc_Click);
+            // 
+            // btnTiep
+            // 
+            this.btnTiep.Location = new System.Drawing.Point(124, 19);
+            this.btnTiep.Name = "btnTiep";
+            this.btnTiep.Size = new System.Drawing.Size(75, 23);
+            this.btnTiep.TabIndex = 1;
+            this.btnTiep.Text = "Tiếp";
+            this.btnTiep.UseVisualStyleBackColor = true;
+            this.btnTiep.Click += new System.EventHandler(this.btnTiep_Click);
+            // 
+            // btnThanhToan
+            // 
+            this.btnThanhToan.Location = new System.Drawing.Point(10, 19);
+            this.btnThanhToan.Name = "btnThanhToan";
+            this.btnThanhToan.Size = new System.Drawing.Size(75, 23);
+            this.btnThanhToan.TabIndex = 0;
+            this.btnThanhToan.Text = "Thanh Toán";
+            this.btnThanhToan.UseVisualStyleBackColor = true;
+            this.btnThanhToan.Click += new System.EventHandler(this.btnThanhToan_Click);
+            // 
+            // frmMain
+            // 
+            this.AcceptButton = this.btnThanhToan;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnKetThuc;
+            this.ClientSize = new System.Drawing.Size(389, 293);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chương trình tính tiền bán hàng";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtThanhTien;
+        private System.Windows.Forms.TextBox txtDonGia;
+        private System.Windows.Forms.TextBox txtSoLuong;
+        private System.Windows.Forms.TextBox txtTenHang;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnKetThuc;
+        private System.Windows.Forms.Button btnTiep;
+        private System.Windows.Forms.Button btnThanhToan;
+    }
+}
+
---
+++ Tuan3/Module3/Bai_1/Bai_1/frmMain.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai_1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThanhToan_Click(object sender, EventArgs e)
+        {
+            // khai báo biến cục bộ
+            int intSoLuong = 0;
+            decimal decDonGia = 0, decThanhTien = 0;
+
+            //kiểm tra giá trị rỗng
+            if (txtSoLuong.Text == "")
+            {
+                MessageBox.Show("Chưa nhập số lượng hàng");
+                txtSoLuong.Focus();
+            }
+            else if (txtDonGia.Text == "")
+            {
+                MessageBox.Show("Chưa nhập đơn giá");
+                txtDonGia.Focus();
+            }
+            else
+            {
+                //nếu dữ liệu khác rỗng, gán vào biến
+                intSoLuong = Convert.ToInt32(txtSoLuong.Text);
+                decDonGia = Convert.ToDecimal(txtDonGia.Text);
+                //kiểm tra dữ liệu hợp lệ
+                if (intSoLuong <= 0)
+                {
+                    MessageBox.Show("Số lượng hàng > 0");
+                    txtSoLuong.SelectAll();
+                    txtSoLuong.Focus();
+                }
+                else if (decDonGia <= 0)
+                {
+                    MessageBox.Show("Đơn giá > 0");
+                    txtDonGia.SelectAll();
+                    txtDonGia.Focus();
+                }
+                else
+                {
+                    //tính thành tiền
+                    decThanhTien = intSoLuong * decDonGia;
+                    //xuất kết quả ra label
+                    txtThanhTien.Text = decThanhTien.ToString();
+                }
+            }
+        }
+        //chỉ cho nhập số vào textbox
+        private void txtSoLuong_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 'A' && e.KeyChar <= 'Z') || (e.KeyChar >= 'a' && e.KeyChar <= 'z')) e.Handled = true;
+        }
+
+        private void btnKetThuc_Click(object sender, EventArgs e)
+        {
+            DialogResult dl;
+            dl = MessageBox.Show("Có chắc chắn là bạn muốn đóng ứng dụng?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dl == DialogResult.Yes) Close();
+        }
+
+        private void btnTiep_Click(object sender, EventArgs e)
+        {
+            txtTenHang.Clear();
+            txtSoLuong.Clear();
+            txtDonGia.Clear();
+            txtThanhTien.Clear();
+            txtTenHang.Focus();
+        }
+    }
+}
